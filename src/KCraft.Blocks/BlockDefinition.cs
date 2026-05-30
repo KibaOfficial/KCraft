46da42b48a6f3c4100858b674102324529cc8cc3
KibaOfficial <kiba@kibaofficial.net>
2026-05-31 01:38:22 +0200

    feat(blocks): add BlockDefinition and BlockRegistry for block textures

---
+++ src/KCraft.Blocks/BlockDefinition.cs
@@ -0,0 +1,10 @@
+// Copyright (c) 2026 KibaOfficial
+// All rights reserved.
+
+namespace KCraft.Blocks;
+public sealed class BlockDefinition
+{
+  public string TextureTop { get; set; } = "dirt";
+  public string TextureSide { get; set; } = "dirt";
+  public string TextureBottom { get; set; } = "dirt";
+}
---
+++ src/KCraft.Blocks/BlockRegistry.cs
@@ -0,0 +1,19 @@
+// Copyright (c) 2026 KibaOfficial
+// All rights reserved.
+
+using KCraft.Blocks;
+namespace KCraft.Blocks;
+
+public static class BlockRegistry
+{
+  public static readonly Dictionary<Block, BlockDefinition> Definitions = new()
+  {
+    [Block.Grass] = new() {
+      TextureTop    = "grass_block_top",
+      TextureSide   = "grass_block_side",
+      TextureBottom = "dirt",
+    },
+    [Block.Dirt]  = new() { TextureTop = "dirt", TextureSide = "dirt", TextureBottom = "dirt" },
+    [Block.Stone] = new() { TextureTop = "stone", TextureSide = "stone", TextureBottom = "stone" },
+  };
+}
